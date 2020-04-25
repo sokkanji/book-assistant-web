1c2dc6cfb6439716e08f62919e9dd303aa3227a5
sokkanji <s2018s07@e-mirim.hs.kr>
2020-04-25 18:42:57 +0900

    JQuery nav 추가

---
+++ src/js/completed.js
@@ -0,0 +1,22 @@
+function headerCompleted() {
+    $("#header").html();
+    $(document).on('mouseover', '.dropdown>li', function () {
+        $('.m2').stop().slideDown(200);
+        $('.menubar').css('height', '225px').css('border-bottom', 'none');
+    });
+    $(document).on('mouseover', '.m2', function () {
+        $('.m2').stop().slideDown(200);
+    });
+    $(document).on('mouseout', '.m2', '.dropdown>li', function () {
+        $('.m2').stop().slideUp(170);
+        $('.menubar').css('height', '110px').css('border-bottom', '1px solid #E6E6E6');
+    });
+}
+
+function contentsCompleted() {
+    $("#contents").html();
+}
+
+function footerCompleted() {
+    $("#footer").html();
+}
