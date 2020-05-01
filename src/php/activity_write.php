7dee54ca9425423c83b11e9bd35af7b5199b1d42
sokkanji <s2018s07@e-mirim.hs.kr>
2020-05-01 14:24:00 +0900

    activity 게시판 목록 php 연결

--- src/php/activity_write.php
+++ src/php/activity_write.php
@@ -1,17 +1,23 @@
 <?php
 require_once("dbconfig.php");
-
-// $id=$_POST['id'];
 $a_title=$_POST['a_title'];
 $a_content=$_POST['a_content'];
+// $a_id=$_POST['a_id'];
 $a_date=date("Y-m-d");
 
-$sql = 'INSERT INTO activity_write(a_title, a_content, a_date) VALUES("' . $a_title . '", "' . $a_content . '","' . $a_date . '")';
+$sql = 'INSERT INTO activity(a_title, a_content, a_date) VALUES("' . $a_title . '", "' . $a_content . '","' . $a_date . '")';
 
 $result = mysqli_query($con, $sql);
-if($result) $msg = "정상적으로 글이 등록되었습니다.";
-else $msg = "글을 등록하지 못했습니다.";
+if($result){ 
+	$msg = "정상적으로 글이 등록되었습니다.";
+	// $a_no = $db->insert_id;
+	echo "<script>alert('$msg'); 
+	document.location.href='../html/review/activity.php'; 
+	</script>";
+} else{
+	$msg = "글을 등록하지 못했습니다.";
+	echo "<script>alert('$msg'); history.back();</script>";
+} 
 
-echo "<script>alert('$msg');</script>";
 mysqli_close($con);
 ?>
--- src/php/dbconfig.php
+++ src/php/dbconfig.php
@@ -1,5 +1,6 @@
 <?php
     $con = mysqli_connect("localhost", "root", "mirim2", "book");
+
     if($con){
         echo "<script>console.log('성공');</script>";
     } else {
