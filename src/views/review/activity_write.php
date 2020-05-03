c8d836859d7d8f8455ed98120f9a253d070a6b72
sokkanji <s2018s07@e-mirim.hs.kr>
2020-05-03 12:41:15 +0900

    activity_write php, report htm 수정

--- src/views/review/activity_write.php
+++ src/views/review/activity_write.php
@@ -1,22 +1,21 @@
 <?php
 require_once("../base/dbconfig.php");
+
 $a_title=$_POST['a_title'];
 $a_content=$_POST['a_content'];
-// $a_id=$_POST['a_id'];
-$a_date=date("Y-m-d");
+// $u_name=$_POST['u_name'];
+$a_date=date("Y-m-d");                                                                                                                                                                                                                              
 
 $sql = 'INSERT INTO activity(a_title, a_content, a_date) VALUES("' . $a_title . '", "' . $a_content . '","' . $a_date . '")';
 
 $result = mysqli_query($con, $sql);
+
 if($result){ 
-	$msg = "정상적으로 글이 등록되었습니다.";
-	// $a_no = $db->insert_id;
-	echo "<script>alert('$msg'); 
-	document.location.href='../html/review/activity.php'; 
+	echo "<script>alert('정상적으로 글이 등록되었습니다.'); 
+	document.location.href='../review/activity.php'; 
 	</script>";
 } else{
-	$msg = "글을 등록하지 못했습니다.";
-	echo "<script>alert('$msg'); history.back();</script>";
+	echo "<script>alert('글을 등록하지 못했습니다.');</script>";	
 } 
 
 mysqli_close($con);
