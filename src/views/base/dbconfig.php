d2af321502d43abf9118d71bc8f6d7cc09d5a8b2
sokkanji <sskkanji@gmail.com>
2020-05-11 00:28:43 +0900

    login, logout, signup, sesstion 구현

--- src/views/base/dbconfig.php
+++ src/views/base/dbconfig.php
@@ -1,12 +1,10 @@
 <?php
-    ini_set('display_errors', 1); 
-    ini_set('error_reporting', E_ALL);
-
     $con = mysqli_connect("localhost", "root", "mirim2", "book");
+
     if (mysqli_connect_errno()) {
       printf("Connect failed: %s\n", mysqli_connect_error());
       exit();
-  }
+    }
 
     if($con){
       echo "<script>console.log('성공');</script>";
