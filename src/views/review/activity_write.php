b86a7ba45c21c085bf5839c4b47e8f24b744ee13
sokkanji <sskkanji@gmail.com>
2020-05-16 22:12:37 +0900

    게시판 글 가상번호, 제목 substr 추가

--- src/views/review/activity_write.php
+++ src/views/review/activity_write.php
@@ -3,16 +3,19 @@ require_once("../base/dbconfig.php");
 
 $a_title=$_POST['a_title'];
 $a_content=$_POST['a_content'];
-$a_date=date("Y-m-d");                                                                                                                                                                                                                              
+$a_date=date("Y-m-d");                          
+
+$mq = 'ALTER TABLE activity auto_increment = 1'; //auto_increment 값 초기화
+$result1 = mysqli_query($con, $mq);
 
 $sql = 'INSERT INTO activity(a_title, a_content, u_name, a_date) VALUES("' . $a_title . '", "' . $a_content . '","' . $_SESSION['u_name'] .'","' . $a_date . '")';
 
-$result = mysqli_query($con, $sql);
+$result2 = mysqli_query($con, $sql);
 if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-if($result){ 
+if($result2){ 
 	echo "<script>alert('정상적으로 글이 등록되었습니다.'); 
 	location.href='../review/activity.php'; 
 	</script>";
