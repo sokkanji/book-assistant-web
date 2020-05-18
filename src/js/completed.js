dd5b7b023739617d25e32515caf5d1373868a34e
sokkanji <s2018s07@e-mirim.hs.kr>
2020-05-19 01:55:00 +0900

    nav css 수정 주석 제거

--- src/js/completed.js
+++ src/js/completed.js
@@ -2,14 +2,13 @@ headerCompleted = () => {
     $("#header").html();
     $(document).on('mouseover', '.dropdown>li', function () {
         $('.m2').slideDown(200);
-        $('.menubar').css('height', '225px').css('border-bottom', 'none');
+        $('.menubar').css('height', '225px').css('box-shadow', 'none');
     });
     $(document).on('mouseover', '.m2', function () {
         $('.m2').stop().slideDown(200);
     });
     $(document).on('mouseout', '.m2', '.dropdown>li', function () {
         $('.m2').stop().slideUp(170);
-        // $('.menubar').css('height', '110px').css('border-bottom', '1px solid #E6E6E6');
         $('.menubar').css('height', '110px').css('box-shadow', '0px 1.4px 0px 0px rgba(0, 0, 0, 0.08)');
     });
 }
