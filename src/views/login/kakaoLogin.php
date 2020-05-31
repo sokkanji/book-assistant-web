02f964458bc7263286c3e345db47f50feba3c474
sokkanji <s2018s07@e-mirim.hs.kr>
2020-05-31 16:02:37 +0900

    news 구현 및 introduce css 수정

--- src/views/login/kakaoLogin.php
+++ src/views/login/kakaoLogin.php
@@ -5,9 +5,9 @@ $_SESSION['userId']=$_GET['userId'];
 $_SESSION['u_name']=$_GET['userNickName'];
 $_SESSION['email']=$_GET['userEmail'];
 
-echo "<script>alert('1>>".$_SESSION['userId'] . "'); </script>";
-echo "<script>alert('2>>".$_SESSION['u_name'] . "'); </script>";
-echo "<script>alert('3>>".$_SESSION['email'] . "'); </script>";
+// echo "<script>alert('1>>".$_SESSION['userId'] . "'); </script>";
+// echo "<script>alert('2>>".$_SESSION['u_name'] . "'); </script>";
+// echo "<script>alert('3>>".$_SESSION['email'] . "'); </script>";
 
 echo "<script>location.href='../base/index.html'</script>";
 ?>
