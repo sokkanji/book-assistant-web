755bc0350c7956f936bfb812ac9099c2e92201d1
sokkanji <sskkanji@gmail.com>
2020-07-06 10:21:36 +0900

    서울 독립서점 좌표값 추가 ajax 중복 이벤트 방지 콘솔에 문구 추가

--- book_result.php
+++ book_result.php
@@ -3,7 +3,7 @@ header("Content-Type:application/json");
 require_once("./dbconfig.php");
 $keyword = $_GET['keyword'];
 
-$sql = "SELECT * FROM book_search WHERE b_title LIKE '%$keyword%'"; 
+$sql = "SELECT * FROM book_search WHERE (b_title LIKE '%$keyword%') OR (writer LIKE '%$keyword%') OR (publisher LIKE '%$keyword%') OR (bookstore LIKE '%$keyword%')"; 
 $result = mysqli_query($con, $sql);
 if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -12,6 +12,7 @@ if (mysqli_connect_errno()){
 $o = array();
 while($board = mysqli_fetch_object($result)){
     $t = new stdClass();
+    $t->b_no = $board->b_no;
     $t->b_title = $board->b_title;
     $t->writer = $board->writer;
     $t->publisher = $board->publisher;
